0130daf2af2b9174d27f88a3b33e4f3fb108eab0
WestonJN <westonnyirenda@gmail.com>
2020-05-06 21:35:20 +0200

    creating movements of tetrominos

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import { createStage } from '../gameHelpers';
+
 //styled compenets
 import {StyledTetrisWrapper, StyledTetris} from './styles/StyledTetris'
 
@@ -12,17 +14,49 @@ import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
 
+//Droptime- sets the timer for tetros drop
+//gameover- ends game
+
 const Tetris = () => {
   const [dropTime, setDropTime] = useState(null);
-  const [gameOver, stGameOver] = useState(false); 
+  const [gameOver, setGameOver] = useState(false); 
 
-  const [player] = usePlayer();
+  const [player, updatePlayerPos, resetPlayer] = usePlayer();
   const [stage, setStage] = useStage(player);
 
   console.log('re-render');
 
-    return(
-        <StyledTetrisWrapper>
+  const movePlayer = dir => {
+    updatePlayerPos({x: dir, y: 0});
+  }
+
+  const startGame = () => {
+    setStage(createStage());
+    resetPlayer();
+  }
+
+  const drop = () => {
+    updatePlayerPos({x: 0, y: 1, collided: false})
+  }
+
+  const dropPLayer = () => {
+    drop();
+  }
+
+  const move = ({ keyCode }) => {
+    if (!gameOver){
+      if(keyCode === 37) {
+        movePlayer(-1);
+      }else if (keyCode === 39){
+        movePlayer(1);
+      }else if (keyCode === 40){
+        dropPLayer();
+      }
+    }
+  }
+
+  return(
+        <StyledTetrisWrapper role= "button" tabIndex="0" onKeyDown={e => move(e)}>
           <StyledTetris>
             <Stage stage={stage} />
             <aside>
@@ -36,7 +70,7 @@ const Tetris = () => {
               </div>
               )}
               
-              <StartButton />
+              <StartButton onClick={startGame} />
             </aside>
             </StyledTetris>
         </StyledTetrisWrapper>
--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,10 +1,35 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { createStage } from '../gameHelpers';
 
 //generate initail stage of game
 //clean stage
-export const useStage = () => {
+export const useStage = (players, resetPlayer) => {
     const[stage, setStage] = useState(createStage());
+
+    useEffect(() => {
+        const updateStage = prevStage => {
+            //First clear it from previous render
+            const newStage = prevStage.map(row =>
+                row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)),
+                );
+
+                //then draw the tetromino
+                player.tetromino.forEach((row, y) =>{
+                    row.forEach((value, x) => {
+                        if (value !== 0){
+                            newStage[y + player.pos.y][x + player.pod.x] =[
+                                value,
+                                `$[player.collided ? 'merged' : 'clear']`,
+                            ]
+                        }
+                    })
+
+                })
+        };
+
+        setStage(prev => updateStage(prev))
+
+    }, [])
 
     return[stage, setStage]; 
 }
